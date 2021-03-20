4d910a9eaca0474061e53d09557e98b1c967ce08
Dean-22 <79496319+Dean-22@users.noreply.github.com>
2021-03-20 15:12:21 +0800

    Update SimpleBiodata.java

--- SimpleBiodata.java
+++ SimpleBiodata.java
@@ -1,1 +1,33 @@
-file
+package com.biodata;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+
+@WebService
+public class SimpleBiodata {
+    @WebMethod
+    
+    public String Name(String name){
+        return name;
+    }
+    @WebMethod
+    public String Address(String add){
+        return add;
+    }
+    @WebMethod
+    public String DateOfBirth(String dobirth){
+        return dobirth;
+    }
+    @WebMethod
+    public String BirthPlace(String bplace){
+        return bplace;
+    }
+    @WebMethod
+    public String Age(String age){
+        return age;
+    }
+    @WebMethod
+    public String ContactNo(String num){
+        return num;
+    }
+}
